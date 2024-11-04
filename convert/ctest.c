1d650206f8a1f3d8bca9136d761733cbcd44f790
Tridib Banik <banikt@mcmaster.ca>
2024-11-04 17:13:11 -0500

    Add convert/ directory from 2XC3-Code

---
+++ convert/ctest.c
@@ -0,0 +1,12 @@
+/* This is a stub program. You should remove this, but you can compile it 
+ * and then try test_script with it. All the test cases will fail.
+ *
+ * Sam Scott, McMaster University, 2024 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	puts("This is just a test!");
+}
